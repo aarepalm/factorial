b4e2c0ed5d6f6f41bbea409fed5445c9fe36edf0
Aare Palm <aarepalm@gmail.com>
2019-02-17 13:30:11 +0200

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -8,6 +8,18 @@ class Factorial {
 		
 		return input * factorial(input - 1);
 	}
+
+	long factorialNonRecursive(long input) {
+		if (input < 2)
+			return 1;
+
+		long result = input;
+		while (input > 2) {
+			result = result * --input;
+		}
+		return result;
+	}
+
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome stranger.");
@@ -27,7 +39,17 @@ class Factorial {
 				System.out.println("Negative number, not acceptable");
 			}
 			else {
-				System.out.println("Result " + obj.factorial(input));
+				System.out.println("Result (recursive)      " + obj.factorial(input));
+				System.out.println("Result (non-recursive)  " + obj.factorialNonRecursive(input));
+				// Aare comment: The body of the factorialNonRecursive could also be directly here.
+				// Like that(you can test it by commenting out last line and commenting in the following section) :
+				/*
+				long result = input;
+				while (input > 2) {
+					result = result * --input;
+				}
+				System.out.println("Result (non-recursive)  " + result);
+				*/
 			}
 		} while (exitCondition == false);
 	}
